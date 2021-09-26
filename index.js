d17cba948bc901924fd1c6dfc5894d8df2d42b76
Wallaci Aparecido <wallaci@MacBook-Pro-de-Wallaci.local>
2021-09-26 11:10:06 -0300

    Adicionando o cors

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
+
+app.use(
+    cors({
+        credentials: true,
+        origin: true
+    })
+)
+
+app.options('*', cors())
+
+app.get('/api/produtos', (req, res) =>{
+    res.send("Bem vindo")
+})
 
 app.listen(3000, () => {
     console.log('API iniciando na porta 3000')
